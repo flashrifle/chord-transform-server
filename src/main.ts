7d66a7418991cc15a6e32505a75ab09366fd20e6
flashrifle <flashrifle@gmail.com>
2024-08-02 15:05:30 +0900

    FEAT: CORS 기본코드 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // cors 설정
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
+
   await app.listen(8000);
 }
 bootstrap();
